1ef5abc4098eabf4d6dcede01a9cb786f283b61b
Ryan Goldstein <ryanisaacg@gmail.com>
2017-07-28 22:38:03 -0400

    Added a very basic sprite

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import pyglet
 
 window = pyglet.window.Window()
 
+image = pyglet.image.load('img.png')
+sprite = pyglet.sprite.Sprite(image, x = 50, y = 50)
+
 @window.event
 def on_draw():
     window.clear()
+    sprite.draw()
 
 pyglet.app.run()
